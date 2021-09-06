4d7e418ade2e5155e96cb3e0558a8278fe7baf44
zahid <zahid@LAPTOP-OQANSVI8>
2021-09-06 11:46:12 +0400

    Add project files.

---
+++ WPF_Exercise/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Exercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public Random Random { get; set; } = new Random();
+        public Grid children { get; }
+
+        private SolidColorBrush NewColor()
+        {
+            var r = Convert.ToByte(Random.Next(0, 255));
+            var g = Convert.ToByte(Random.Next(0, 255));
+            var b = Convert.ToByte(Random.Next(0, 255));
+
+            return  new SolidColorBrush(Color.FromRgb(r, g, b)); 
+        }
+
+        private void LeftMouse_Clicked(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Background = NewColor();
+            MessageBox.Show($"{btn.Content}'s color is changed" );
+        }
+
+        private void RightMouse_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+            GridBtn.Children.Remove(btn);
+            Title = btn.Content.ToString();
+        }
+    }
+}
